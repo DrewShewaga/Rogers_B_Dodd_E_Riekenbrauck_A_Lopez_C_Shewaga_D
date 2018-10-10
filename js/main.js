da086014ee0fe614f3b549c61f70d15a35a8f29f
DrewShewaga <cierenius@gmail.com>
2018-10-10 12:03:47 -0400

    Mobile Boxed Layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 // JavaScript Document
 (() => {
+//VARIABLE STACK
+let navIcon = document.querySelector('.navIcon'),
+    navList = document.querySelector('.navList');
+
+
+
+//FUNCTIONS
+function toggleMenu() {
+    if (navList.classList.value == 'navList hidden') {
+        navList.classList.remove('hidden');
+    }
+    else {
+        navList.classList.add('hidden');
+    }
+}
+
+
+//EVENT HANDLING
+navIcon.addEventListener('click', toggleMenu);
+
 
 })();
