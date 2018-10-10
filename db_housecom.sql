aa7c71ceca952a7bfc5c18d6c0808506ab716996
Brendan Rogers <brendanr11@gmail.com>
2018-10-10 16:44:07 -0400

    Added hero image

--- db_housecom.sql
+++ db_housecom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Oct 10, 2018 at 03:28 PM
+-- Generation Time: Oct 10, 2018 at 08:31 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
@@ -30,7 +30,7 @@ CREATE TABLE `tbl_desc` (
 --
 
 INSERT INTO `tbl_desc` (`id`, `product_desc`) VALUES
-(1, 'the smartest piece in town');
+(1, 'climate controlled');
 
 -- --------------------------------------------------------
 
@@ -48,7 +48,7 @@ CREATE TABLE `tbl_product` (
 --
 
 INSERT INTO `tbl_product` (`id`, `product_name`) VALUES
-(1, 'HouseCom ThermoSecure');
+(1, 'ThermoSecure');
 
 -- --------------------------------------------------------
 
