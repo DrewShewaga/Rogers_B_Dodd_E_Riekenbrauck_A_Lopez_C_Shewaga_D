90d62c8b0394e856e450119b33bb1012c2fd6a8b
Brendan Rogers <brendanr11@gmail.com>
2018-10-10 13:52:15 -0400

    Implemented Pan's PHP code. Spent like 2 hours working out the kinks. It's ready to place words on the screen. It just needs to know where those words are going

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
 // JavaScript Document
 (() => {
 
+
+
+function getResults() {
+
+	console.log('hit get results');
+
+	fetch('./product/read.php')
+	.then(res => res.json())
+	.then(data => {
+	  console.log(data);
+
+
+	  data.forEach((item, index) => {
+
+	  	// THIS IS THE NAME OF OUR PRODUCT
+	  	// SELECT THE PROPER QUERY WHEN ITS AVAILABLE
+	    // document.querySelector("#test1").textContent = item.product_name;
+
+	    // every Linked Table needs to be accsessed via another Loop, due to how Pan 
+	    item.product_desc.forEach(desc => {
+	    	// THIS IS THE DESCRIPTION OF OUR PRODUCT
+		  	// SELECT THE PROPER QUERY WHEN ITS AVAILABLE
+			// document.querySelector("#test2").textContent += desc.product_desc;
+        });
+
+
+	  });
+	})
+	.catch(function(error) {
+	  console.log(error);
+	});
+}
+
+// fire off the fetch call
+getResults();
+
 })();
